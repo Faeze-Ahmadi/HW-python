aa4462c38e0124479630f0703687b481511b5851
Faeze <faezeahmadi7173@gmail.com>
2024-07-01 20:29:11 +0330

    HW14

---
+++ HW-14.py
@@ -0,0 +1,9 @@
+#HW14
+
+def count_word_adalat(sentence):
+    return sentence.count('عدالت')
+
+# Example usage
+sentence = "عدالت عدالت عدالت"  # You can change the sentence
+count = count_word_adalat(sentence)
+print(f"The word 'عدالت' appears {count} times in the sentence.")
